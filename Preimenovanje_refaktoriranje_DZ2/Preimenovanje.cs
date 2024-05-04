9955a023d0a6e1dea92428a09b5f2fb57b394e50
nsuci <nsuci@LAPTOP-NS>
2024-05-04 11:02:28 +0200

    Add project files.

---
+++ Preimenovanje_refaktoriranje_DZ2/Preimenovanje.cs
@@ -0,0 +1,106 @@
+﻿//Preimenovanje:
+//Zadatak 2:
+class Product
+{
+    public string Name { get; private set; } 
+    public decimal Price { get; private set; } 
+    public bool IsStocked { get; set; } 
+
+    public Product(string name, decimal price)
+    {
+        this.Name = name;
+        this.Price = price;
+        this.IsStocked = false;
+    }
+}
+
+class Inventory
+{
+    List<Product> products; 
+
+    public Inventory(List<Product> products) 
+    {
+        this.products = products;
+    }
+
+    public void Restock(Product product)
+    {
+        foreach (Product prod in products)
+        { 
+            if (product == prod)
+                prod.IsStocked = true;
+        }
+    }
+    public void RemoveSoldOut()
+    { 
+        products.RemoveAll(product => product.IsStocked == false);
+    }
+}
+
+//Zadatak 4:
+public class Note
+{
+    public string Title { get; set; }
+    public string Text { get; set; }
+    public DateTime Date { get; private set; }
+
+    public Note(string title, string text)
+    {
+        Title = title;
+        Text = text;
+        Date = DateTime.Now;
+    }
+}
+
+public class Notebook
+{
+    public string Author { get; private set; }
+    public List<Note> notes;
+
+    public Notebook(string author)
+    {
+        Author = author;
+        notes = new List<Note>();
+    }
+
+    public void AddNote(Note note)
+    {
+        notes.Add(note);
+    }
+}
+
+//Zadatak 5:
+
+public class Location
+{
+    public DateTime CreatedAt { get; private set; } 
+    public double Latitude { get; private set; } 
+    public double Longitude { get; private set; } 
+
+    public Location(double latitude, double longitude)
+    {
+        Latitude = latitude;
+        Longitude = longitude;
+        CreatedAt = DateTime.Now;
+    }
+}
+
+public class PathManager
+{
+    private List<Location> locations; 
+
+    public PathManager()
+    {
+        locations = new List<Location>();
+    }
+
+    public void AddNewLocation(Location location)
+    {
+        locations.Add(location);
+    }
+
+    public void RemoveLocation(Location location)
+    {
+        locations.Remove(location);
+    }
+}
---
+++ Preimenovanje_refaktoriranje_DZ2/Refaktoriranje.cs
@@ -0,0 +1,88 @@
+﻿//Refaktoriranje
+
+//Zadatak 2:
+using System.Runtime.CompilerServices;
+
+class Average
+{
+    List<double> CreateListOfAverages(List<double[]> arrays)
+    {
+        List<double> averages = new List<double>(); 
+        
+        foreach (double[] array in arrays)
+        {
+            averages.Add(CalculateAverage(array));
+        }
+        return averages;
+    }
+
+    public double CalculateAverage(double[] array)
+    {
+        double average = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            average += array[i];
+
+        }
+        return average/array.Length;
+        
+    }
+}
+
+//Zadatak 4:
+class CetvrtiZadatak
+{
+    public static List<char> CountUniqueChars(string text)
+    {
+        List<char> chars = new List<char>();
+        for (int i = 0; i < text.Length; i++)
+        {
+            if(CountCharOccurrence(text, text[i]) == 1)
+            {
+                chars.Add(text[i]);
+            }
+            
+        }
+        return chars;
+    }
+
+    public static int CountCharOccurrence(string text, char character)
+    {
+        int occurrenceCount = 0;
+        for (int j = 0; j < text.Length; j++)
+        {
+            if (character == text[j])
+            {
+                occurrenceCount++;
+            }
+        }
+        return occurrenceCount;
+    }
+}
+
+//Zadatak 5:
+class PetiZadatak
+{
+    public List<string> FindPalindromes(List<string> strings)
+    {
+        List<string> palindromes = new List<string>();
+        if (strings == null) return palindromes;
+
+        foreach (string str in strings)
+        {
+            if (IsPalindrome(str))
+            {
+                palindromes.Add(str);
+            }
+        }
+        return palindromes;
+    }
+
+    public bool IsPalindrome(string str)
+    {
+        string trimmedString = str.Replace(" ", "").ToLower();
+        string reversedString = new string(trimmedString.Reverse().ToArray());
+
+        return trimmedString.Equals(reversedString);
+    }
+}
